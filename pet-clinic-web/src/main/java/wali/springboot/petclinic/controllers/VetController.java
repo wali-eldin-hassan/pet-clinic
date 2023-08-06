5b4f27443e2a66ba2520229652c43b30f64798b3
wali-eldin-hassan <creaspo6@gmail.com>
2023-08-06 11:26:16 +0300

    Added Vets Controllers and Views

---
+++ pet-clinic-web/src/main/java/wali/springboot/petclinic/controllers/VetController.java
@@ -0,0 +1,14 @@
+package wali.springboot.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets","vets/index","vets/index.html"})
+    public  String index()
+    {
+        return  "vets/index";
+    }
+}
