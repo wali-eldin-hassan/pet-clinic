c40c29272df151b4cab820fe585a19baa55ffe6b
wali-eldin-hassan <creaspo6@gmail.com>
2023-08-16 16:28:29 +0300

    Convert Owners to jpa entities

--- pet-clinic-data/src/main/java/wali/springboot/petclinic/models/Person.java
+++ pet-clinic-data/src/main/java/wali/springboot/petclinic/models/Person.java
@@ -1,11 +1,17 @@
 package wali.springboot.petclinic.models;
 
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
 /**
  * Created by Wali
  **/
+@MappedSuperclass
 public class Person extends  BaseEntity {
+    @Column(name = "first_name")
     private  String firstName;
+    @Column(name = "last_name")
     private  String lastName;
 
     public void setFirstName(String firstName) {
