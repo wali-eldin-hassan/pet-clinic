7074ebd6ebbe447b0e1c21ac5bd0840370ea1836
wali-eldin-hassan <creaspo6@gmail.com>
2023-08-18 12:27:02 +0300

    Create visit Map Services

---
+++ pet-clinic-data/src/main/java/wali/springboot/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,39 @@
+package wali.springboot.petclinic.services.map;
+
+import wali.springboot.petclinic.models.Visit;
+import wali.springboot.petclinic.services.VisitServices;
+
+import java.util.Set;
+
+public class VisitMapService  extends AbstractService<Visit,Long> implements VisitServices {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet()==null || visit.getPet().getOwner()==null||visit.getPet().getId()==null||
+                visit.getPet().getOwner().getId()==null)
+        {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteBYId(id);
+    }
+}
