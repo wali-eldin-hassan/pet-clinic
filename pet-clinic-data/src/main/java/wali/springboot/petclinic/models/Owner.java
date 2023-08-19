c8cec02873999a8c1b1b45364a791f698828c33c
wali-eldin-hassan <creaspo6@gmail.com>
2023-08-19 16:55:20 +0300

    Add project Lombok and refactor

--- pet-clinic-data/src/main/java/wali/springboot/petclinic/models/Owner.java
+++ pet-clinic-data/src/main/java/wali/springboot/petclinic/models/Owner.java
@@ -1,55 +1,36 @@
 package wali.springboot.petclinic.models;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Setter
+@Getter
+@NoArgsConstructor
 @Table(name = "owners")
 public class Owner extends Person{
+
+    @Builder
+    public Owner( Long id,String firstName,String lastName,String city, String phone, Set<Pet> pets, String address) {
+        super(id,firstName,lastName);
+        this.city = city;
+        this.phone = phone;
+        this.pets = pets;
+        this.address = address;
+    }
 
     @Column(name = "city")
     private  String city;
     @Column(name = "phone")
-
     private String phone;
     @OneToMany(cascade = CascadeType.ALL,mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
     @Column(name = "address")
-
     private  String address;
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public Set<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(Set<Pet> pets) {
-        this.pets = pets;
-    }
 
 
 
--- pet-clinic-data/src/main/java/wali/springboot/petclinic/models/Pet.java
+++ pet-clinic-data/src/main/java/wali/springboot/petclinic/models/Pet.java
@@ -1,11 +1,18 @@
 package wali.springboot.petclinic.models;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "pets")
 public class Pet  extends  BaseEntity{
     @Column(name = "name")
@@ -17,49 +24,10 @@ public class Pet  extends  BaseEntity{
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
-
     @Column(name = "birth_date")
     private LocalDate birthDate;
     @OneToMany(cascade = CascadeType.ALL,mappedBy = "pet")
-
     private Set<Visit> visits=new HashSet<>();
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-    public PetType getPetType() {
-        return petType;
-    }
-
-    public void setPetType(PetType petType) {
-        this.petType = petType;
-    }
-
-    public Owner getOwner() {
-        return owner;
-    }
-
-    public void setOwner(Owner owner) {
-        this.owner = owner;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public Set<Visit> getVisits() {
-        return visits;
-    }
-
-    public void setVisits(Set<Visit> visits) {
-        this.visits = visits;
-    }
 }
